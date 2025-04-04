2c0e20f36f4a6157a330c31168ba114e04380ecc
radiasabahat <radiasabahat@gmail.com>
2025-04-04 06:19:31 -0700

    thied commit

--- app.js
+++ app.js
@@ -49,3 +49,73 @@ console.log(tranformtobjects);
 const nums = [10, 20, 55, 40, 70];
 const firstGreaterThan50 = nums.find(num => num > 50);
 console.log(firstGreaterThan50); 
+
+
+
+// destructuring for arrays 
+const [firstName, lastName] = ['john', 'doe'];
+console.log(lastName)
+
+//destructuring for objects
+const {name, age}={
+  name:"alia",
+  age: 33
+
+};
+ console.log(name);
+console.log(age);
+ 
+//destructuring in functions
+function storeOrder({id, currency}){
+  localStorage.setitem('id', id);
+  localStorage.setItem('currency',currency);
+}
+
+//spread operator
+const hobbies= ['cooking', 'singing', 'arts'];
+ const newUserNames= ['lily', 'john'];
+ const mergeNames=[...userName, ...newUserNames]
+ console.log(mergeNames)
+ //spread operators in objet
+ const user={
+  name:'max',
+  age: 22
+ };
+const extendedUser={
+  ...user,
+  standar: 'a-levels',
+  phone: '090078601'
+ 
+};
+console.log(extendedUser)
+
+//functions as values
+function handleTimeout() {
+  console.log('time out')
+};
+const handleTimeout2 =()=>{
+  console.log('time out');
+};  
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+
+setTimeout(() => {
+  console.log('more timing out')
+}, 4000 );
+
+// if we want to custom  a fnction . passing functions as values is not limited to built-in functions  but can be done with all functions or the functon of our own
+function greeter(greetfn){
+  greetfn();
+};
+greeter(()=>console.log('hi'));
+
+//func. inside func
+function init(){
+ function greet(){
+    console.log('hye');
+
+  }
+greet();
+}
+init();
+
